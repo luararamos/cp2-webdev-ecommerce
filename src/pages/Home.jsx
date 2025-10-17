81497678ee498f9cae460136ae30ddadeac14c91
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-16 21:09:07 -0300

    rota correcao

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,24 @@ import Section from "../components/Section";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  const API_URL = import.meta.env.VITE_API_URL;
   const [categories, setCategories] = useState([]);
   const [productsByCategory, setProductsByCategory] = useState({});
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/categories")
+    fetch(`${API_URL}/products/categories`)
       .then((res) => res.json())
       .then((data) => {
         setCategories(data);
         data.forEach((category) => {
-          fetch(`https://fakestoreapi.com/products/category/${encodeURIComponent(category)}`)
+          fetch(`${API_URL}/products/category/${encodeURIComponent(category)}`)
             .then((res) => res.json())
             .then((products) => {
               setProductsByCategory((prev) => ({ ...prev, [category]: products }));
             });
         });
       });
-  }, []);
+  }, [API_URL]);
 
   return (
     <div className="p-6 flex-1 bg-gray-100">
